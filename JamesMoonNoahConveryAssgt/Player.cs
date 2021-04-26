4c04f54cdca3208530c8a9720e6fc22c195dad89
James <james.moon@uon.edu.au>
2021-04-26 14:52:38 +1000

    Added Player class

---
+++ JamesMoonNoahConveryAssgt/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JamesMoonNoahConveryAssgt
+{
+    public class Player
+    {
+        private string strName;
+        private int intScore;
+        private Random rnd = new Random();
+
+        public string getName()
+        {
+            return strName;
+        }
+
+        public void setName(string myname)
+        {
+            strName = myname;
+        }
+
+        public int getScore()
+        {
+            return intScore;
+        }
+
+        public void setScore(int newScore)
+        {
+            intScore = newScore;
+        }
+
+        public Player()
+        {
+            strName = "";
+            intScore = 0;
+        }
+
+        public Player(string newPlayerName)
+        {
+            strName = newPlayerName;
+            intScore = 0;
+        }
+    }
+}
