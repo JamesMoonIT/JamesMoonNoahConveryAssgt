7e21f3cc7c32737eda3b73cb95e207eb4447238d
James <james.moon@uon.edu.au>
2021-04-26 15:36:12 +1000

    Added Game class

---
+++ JamesMoonNoahConveryAssgt/Game.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JamesMoonNoahConveryAssgt
+{
+    class Game
+    {
+        private Player[] numOfPlayers = new Player[2];
+        private Dice diceStats;
+        private int intGoal, intTurn;
+        private bool boolEnd, boolCurrentPlayerWins;
+        private Random rand = new Random();
+
+        public Game(Player playerOne, Player playerTwo, int finalGoal)
+        {
+            int intWhosTurn = rand.Next(0, 1);
+            intGoal = finalGoal;
+            boolEnd = false;
+            diceStats = new Dice();
+            numOfPlayers[0] = playerOne;
+            numOfPlayers[1] = playerTwo;
+            
+            if (intWhosTurn == 0)
+            {
+                intTurn = 0;
+            }
+            else
+            {
+                intTurn = 1;
+            }
+        }
+
+        // public string CountScore() { }
+
+        public Player[] GetPlayers()
+        {
+            return numOfPlayers;
+        }
+
+        public int GetGoal()
+        {
+            return intGoal;
+        }
+
+        public int WhosTurn()
+        {
+            return intTurn;
+        }
+
+        public bool GameOver()
+        {
+            return boolEnd;
+        }
+
+        public bool CurrentPlayerWins()
+        {
+            return boolCurrentPlayerWins;
+        }
+
+
+    }
+}
