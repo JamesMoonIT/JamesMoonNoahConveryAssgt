9179cd71749ee31a0c0f19d12b7b71fed2d036ec
James <james.moon@uon.edu.au>
2021-04-28 15:51:29 +1000

    Working rules

--- JamesMoonNoahConveryAssgt/GroanGame.cs
+++ JamesMoonNoahConveryAssgt/GroanGame.cs
@@ -93,7 +93,7 @@ namespace JamesMoonNoahConveryAssgt
         private void DiceRoll()
         {
             ClearDice();
-            int roll1 = 0, roll2 = 0, result1, result2, dice1, dice2;
+            int roll1 = 0, roll2 = 0, result1, result2;
             for (int iteration = 0; iteration < 10; iteration++)
             {
                 roll1 = rand.Next(1, 7);
@@ -113,6 +113,7 @@ namespace JamesMoonNoahConveryAssgt
             {
                 // if player rolls one 1
                 txtbxRunningScore.Text = "Turn Passed";
+                currentSession.GetCurrentGame().SwitchPlayers();
             }
             else if (result1 == 1 && result2 == 1)
             {
