8faf1e7130fca0d404db33ff5ca6a4b72497d3de
James Moon <c3318556@uon.edu.au>
2021-05-12 12:29:59 +1000

    Commented half of session

--- JamesMoonNoahConveryAssgt/Session.cs
+++ JamesMoonNoahConveryAssgt/Session.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/* 
+    Name: James Moon & Noah Convery
+    Date: 12/5/2021
+    Description: This class stores the session data given through GroanGame for later reference in the program, as well as
+                    extra methods for modifiying and creating session data.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,35 +20,35 @@ namespace JamesMoonNoahConveryAssgt
         private int intPlayerOne, intPlayerTwo;
         private bool boolAi;
 
-        public Session(string strP1Name, string strP2Name, int intScore, bool ai)
+        public Session(string strP1Name, string strP2Name, int intScore, bool ai)               // Creates a session with player data and game data
         {
-            boolAi = ai;
-            intPlayerOne = 0;
-            intPlayerTwo = 0;
-            playerOne = new Player(strP1Name);
-            playerTwo = new Player(strP2Name);
-            gameCurrent = new Game(playerOne, playerTwo, intScore);
+            boolAi = ai;                                                                        // Set to true if an ai is present, false if otherwise
+            intPlayerOne = 0;                                                                   // Total number of wins for player one
+            intPlayerTwo = 0;                                                                   // Total number of wins for player two
+            playerOne = new Player(strP1Name);                                                  // Creates a new player with player one name
+            playerTwo = new Player(strP2Name);                                                  // Creates a new player with player two name
+            gameCurrent = new Game(playerOne, playerTwo, intScore);                             // Creates a new game with player one and two's name, and score
         }
 
-        public void Restart(int iScore)
+        public void Restart(int iScore)                                                         // Creates a new game with player names and current score
         {
-            gameCurrent = new Game(playerOne, playerTwo, iScore);
+            gameCurrent = new Game(playerOne, playerTwo, iScore);                               // Calls new game class with variables
         }
 
-        public bool HasGameEnded()
+        public bool HasGameEnded()                                                              // checks and updates the game if the score has been reached
         {
             int score = gameCurrent.GetGoal();
             if ((GetCurrentGame().GetPlayers()[GetCurrentGame().WhosTurn()].GetScore() + GetCurrentGame().GetRunningScore()) >= score)
-            {
-                if (gameCurrent.WhosTurn() == 0)
+            {                                                                                   // ^ Checks the current players score and cumulative score if it is over the game score
+                if (gameCurrent.WhosTurn() == 0)                                                // if its player one's turn
                 {
-                    PlayerOneWins();
+                    PlayerOneWins();                                                            // increases player one wins by one
                 }
-                else
+                else                                                                            // if its player two's turn
                 {
-                    PlayerTwoWins();
+                    PlayerTwoWins();                                                            // increases player two wins by one
                 }
-                return true;
+                return true;                                                                    // returns that a player has won
             }
             else
             {
